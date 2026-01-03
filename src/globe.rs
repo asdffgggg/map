8ca8064b0c4af0c37b2abcbb9abe7d6d8b5a1c39
Shubi Sahni <shubisahni@icloud.com>
2026-01-03 13:01:16 -0800

    mesh made

--- src/globe.rs
+++ src/globe.rs
@@ -1,23 +1,28 @@
 use std::f32::consts::{PI, TAU};
 
-use bevy::math::vec3;
+use bevy::{
+    asset::RenderAssetUsages,
+    mesh::{Indices, PrimitiveTopology},
+    prelude::*,
+};
 
-pub fn make_globe_mesh() {
+pub fn make_globe_mesh() -> Mesh {
     let image_bytes = include_bytes!("../World_elevation_map.png");
     let img = image::load_from_memory(image_bytes).unwrap().into_luma8();
-    let scale = 10;
+    let scale = 3;
     let width = img.width() as usize / scale;
-    let height = img.height() as usize / scale;
+    let height: usize = img.height() as usize / scale;
     let mut vertices = Vec::with_capacity(width * height);
     let mut normals = Vec::with_capacity(width * height);
     let mut indices = Vec::new();
-    let base_radius = 0.0;
-    let relief = 1.0;
+    let base_radius = 1.0;
+    let relief = 0.3;
     for i in 0..width {
         let x = (i * scale) as u32;
         let theta = i as f32 / width as f32 * TAU;
         for j in 0..height {
-            let k = vertices.len();
+            let k = vertices.len() as u32;
+            let height = height as u32;
             let y = (j * scale) as u32;
             let px = img.get_pixel(x, y).0[0];
             let phi = j as f32 / height as f32 * PI - PI / 2.0;
@@ -32,4 +37,11 @@ pub fn make_globe_mesh() {
             indices.extend([k, k + 1 + height, k + height]);
         }
     }
+    Mesh::new(
+        PrimitiveTopology::TriangleList,
+        RenderAssetUsages::default(),
+    )
+    .with_inserted_attribute(Mesh::ATTRIBUTE_POSITION, vertices)
+    .with_inserted_attribute(Mesh::ATTRIBUTE_NORMAL, normals)
+    .with_inserted_indices(Indices::U32(indices))
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,47 @@ mod globe;
 
 use bevy::prelude::*;
 
+use crate::globe::make_globe_mesh;
+
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .add_systems(Update, rotate)
+        .run();
+}
+
+#[derive(Component)]
+struct Globe;
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn((
+        Globe,
+        Mesh3d(meshes.add(make_globe_mesh())),
+        MeshMaterial3d(materials.add(Color::srgb_u8(30, 121, 160))),
+        Transform::from_scale(Vec3::splat(1.0)),
+    ));
+    // light
+    commands.spawn((
+        PointLight {
+            shadows_enabled: true,
+            ..default()
+        },
+        Transform::from_xyz(4.0, 8.0, 4.0),
+    ));
+    // camera
+    commands.spawn((
+        Camera3d::default(),
+        Transform::from_xyz(0.0, 0.0, 4.0).looking_at(Vec3::ZERO, Vec3::Y),
+    ));
+}
+
+fn rotate(time: Res<Time>, mut query: Query<&mut Transform, With<Globe>>) {
+    for mut t in &mut query {
+        t.rotate_local_y(0.1 * time.delta_secs());
+    }
 }
