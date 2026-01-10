5c4f73475551465ceeb4dc569cf49a755c0ecd97
Shubi Sahni <shubisahni@icloud.com>
2026-01-10 12:58:59 -0800

    mesh works well

--- src/globe.rs
+++ src/globe.rs
@@ -6,30 +6,32 @@ use bevy::{
     prelude::*,
 };
 
+pub const BASE_RADIUS: f32 = 1.0;
+pub const RELIEF: f32 = 0.3;
+
 pub fn make_globe_mesh() -> Mesh {
     let image_bytes = include_bytes!("../World_elevation_map.png");
     let img = image::load_from_memory(image_bytes).unwrap().into_luma8();
-    let scale = 3;
+    let scale = 2;
     let width = img.width() as usize / scale;
     let height: usize = img.height() as usize / scale;
     let mut vertices = Vec::with_capacity(width * height);
     let mut normals = Vec::with_capacity(width * height);
     let mut indices = Vec::new();
-    let base_radius = 1.0;
-    let relief = 0.3;
-    for i in 0..width {
+    for mut i in 0..=width {
+        i %= width;
         let x = (i * scale) as u32;
-        let theta = i as f32 / width as f32 * TAU;
+        let phi = i as f32 / width as f32 * -TAU;
         for j in 0..height {
             let k = vertices.len() as u32;
             let height = height as u32;
             let y = (j * scale) as u32;
             let px = img.get_pixel(x, y).0[0];
-            let phi = j as f32 / height as f32 * PI - PI / 2.0;
-            let r = base_radius + px as f32 / 255.0 * relief;
+            let theta = j as f32 / height as f32 * PI;
+            let r = BASE_RADIUS + px as f32 / 255.0 * RELIEF;
             let x = theta.sin() * phi.cos();
-            let y = theta.sin() * phi.sin();
-            let z = theta.cos();
+            let z = theta.sin() * phi.sin();
+            let y = theta.cos();
             let n = vec3(x, y, z);
             normals.push(n);
             vertices.push(n * r);
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod globe;
 
 use bevy::prelude::*;
 
-use crate::globe::make_globe_mesh;
+use crate::globe::{BASE_RADIUS, RELIEF, make_globe_mesh};
 
 fn main() {
     App::new()
@@ -21,29 +21,52 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
+    //globe
+    let base_color = Color::srgb_u8(70, 220, 90);
     commands.spawn((
         Globe,
         Mesh3d(meshes.add(make_globe_mesh())),
-        MeshMaterial3d(materials.add(Color::srgb_u8(30, 121, 160))),
-        Transform::from_scale(Vec3::splat(1.0)),
+        MeshMaterial3d(materials.add(StandardMaterial {
+            base_color,
+            // vary key PBR parameters on a grid of spheres to show the effect
+            metallic: 0.01,
+            perceptual_roughness: 0.632,
+            ..default()
+        })),
+        Transform::from_scale(Vec3::splat(10.0)),
+    ));
+    //ocean
+    commands.spawn((
+        Mesh3d(
+            meshes.add(
+                Sphere {
+                    radius: BASE_RADIUS + RELIEF * 0.5625,
+                }
+                .mesh()
+                .uv(200, 110),
+            ),
+        ),
+        MeshMaterial3d(materials.add(Color::srgb_u8(13, 86, 185))),
+        Transform::from_scale(Vec3::splat(10.0)),
     ));
     // light
     commands.spawn((
-        PointLight {
+        DirectionalLight {
+            illuminance: 10000.0,
             shadows_enabled: true,
             ..default()
         },
-        Transform::from_xyz(4.0, 8.0, 4.0),
+        Transform::from_xyz(40.0, 8.9771, 40.0).looking_at(Vec3::ZERO, Vec3::Y),
     ));
     // camera
     commands.spawn((
         Camera3d::default(),
-        Transform::from_xyz(0.0, 0.0, 4.0).looking_at(Vec3::ZERO, Vec3::Y),
+        Transform::from_xyz(0.0, 0.0, 40.0).looking_at(Vec3::ZERO, Vec3::Y),
     ));
 }
 
 fn rotate(time: Res<Time>, mut query: Query<&mut Transform, With<Globe>>) {
     for mut t in &mut query {
-        t.rotate_local_y(0.1 * time.delta_secs());
+        t.rotate_local_y(0.4 * time.delta_secs());
     }
 }
